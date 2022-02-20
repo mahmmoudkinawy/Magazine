56490387dab0a9653017b33f690dba1b36d38919
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-20 14:06:48 +0200

    Added create category and it's view

--- Magazine/Controllers/CategoryController.cs
+++ Magazine/Controllers/CategoryController.cs
@@ -6,7 +6,20 @@ public class CategoryController : Controller
     public CategoryController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
 
     public async Task<IActionResult> Index()
+        => View(await _unitOfWork.CategoryRepository.GetAllAsync());
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Category category)
     {
-        return View(await _unitOfWork.CategoryRepository.GetAllAsync());
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.CategoryRepository.Add(category);
+            await _unitOfWork.SaveAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
     }
 }
